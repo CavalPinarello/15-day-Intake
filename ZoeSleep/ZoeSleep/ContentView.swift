60f5508ec10a3692eb004d1d819f95a37ce616f5
CavalPinarello <130845318+CavalPinarello@users.noreply.github.com>
2026-01-23 13:37:07 -0800

    Fix Date conversion for journey start timestamp

--- ZoeSleep/ZoeSleep/ContentView.swift
+++ ZoeSleep/ZoeSleep/ContentView.swift
@@ -701,7 +701,8 @@ struct MainDashboardView: View {
 
                     // Journey start date
                     if let startTimestamp = questionnaireManager.journeyProgress?.startedAt {
-                        Text("Started \(formatJourneyStartDate(Date(timeIntervalSince1970: TimeInterval(startTimestamp))))")
+                        let startDate = Date(timeIntervalSince1970: Double(startTimestamp))
+                        Text("Started \(formatJourneyStartDate(startDate))")
                             .font(.system(size: Typography.caption, design: .rounded))
                             .foregroundColor(theme.textOnCardMuted)
                     }
